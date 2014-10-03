16ea7a3bc36be3dda331fca4000976f815030789
rkempter <renato.kempter@gmail.com>
2014-10-03 17:53:51 +0200

    database stuff

--- instagram_collector/collector.py
+++ instagram_collector/collector.py
@@ -74,7 +74,7 @@ def init_db():
     """
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().execute(f.read())
         db.commit()
 
 def process_geo_location(updates):
