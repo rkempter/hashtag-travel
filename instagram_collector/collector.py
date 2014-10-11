c2550a322156bb475e90f203191d5501ac519864
rkempter <renato.kempter@gmail.com>
2014-10-11 12:49:33 +0200

    neue version of the collector

--- instagram_collector/collector.py
+++ instagram_collector/collector.py
@@ -3,20 +3,16 @@ Author: Renato Kempter, 2014
 Collector for Instagram images containing geo location information.
 """
 from contextlib import closing
-import threading
+import multiprocessing
+import logging
 
 from collections import namedtuple
 from instagram import subscriptions, client
 from flask import Flask, request, g, redirect, Response
 from instagram_collector.config import (CLIENT_SECRET, REDIRECT_URI, CLIENT_ID,
                                         DATABASE, DB_HOST, DB_PASSWORD, DB_USER,
-                                        THRESHOLD, RETURN_URI)
+                                        THRESHOLD, RETURN_URI, ACCESS_TOKEN)
 import MySQLdb
-
-# We need a lock to have synchronized access to
-# the subscriptions_counter, in order to not overwrite the next
-# parameter
-lock = threading.Lock()
 
 # The subscription_counter takes care of memorizing the 'next' and 'last' id of
 # collected medias
@@ -24,6 +20,13 @@ subscriptions_dict = {}
 
 # The web application
 app = Flask(__name__)
+
+# The shared dictionary
+manager = multiprocessing.Manager()
+shared_dict = manager.dict()
+
+# The queue of tasks with pull information
+queue = multiprocessing.Queue()
 
 # The geographical locations that we want to subscribe to
 LOCATIONS = [
@@ -34,13 +37,18 @@ LOCATIONS = [
     { 'lat': 48.822493333, 'long': 2.162536078, 'radius': 5000 },
 ]
 
+def queue_handling():
+    """
+    Handles new objects that arrive in the queue
+    """
+    pass
+
 @app.route('/init')
 def start():
     """
     Define callback for geographical post messages. Connect to the instagram api and
     get an access token.
     """
-    print "initialize"
     g.reactor = subscriptions.SubscriptionsReactor()
     g.reactor.register_callback(subscriptions.SubscriptionType.GEOGRAPHY, process_geo_location)
     g.unauthenticated_api = client.InstagramAPI(
@@ -53,14 +61,13 @@ def subscribe_location():
     """
     Subscribes to all locations and store the subscription id.
     """
-    print "subscribe"
+    api = client.InstagramAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
     for location in LOCATIONS:
-        api = client.InstagramAPI(access_token=g.access_token)
         response = api.create_subscription(
             object='geography', lat=location['lat'], lng=location['long'],
             radius=location['radius'], aspect='media',
             callback_url=RETURN_URI)
-
+        print response
         for el in response.data:
             if 'subscription' in el['type']:
                 subscriptions_dict[el['object_id']] = {'counter': 0}
@@ -88,31 +95,57 @@ def process_geo_location(updates):
     Process a list of updates and add them to subscriptions.
     @Todo: This can be done using multiprocessing as well.
     """
-    for update in updates:
-        subscription_id = update['subscription_id']
-        subscription_val = subscriptions_dict[subscription_id]
-        lock.acquire()
-        try:
-            if 'next' not in subscription_val:
-                subscription_val['next'] = update['object_id']
-            subscription_val['last'] = update['object_id']
-            subscription_val['counter'] += 1
-            if subscription_val['counter'] > THRESHOLD:
-                retrieve_recent_geo(subscription_val)
-        finally:
-            lock.release()
+    print updates
+    #for update in updates:
+    #    subscription_id = update['subscription_id']
+    #    subscription_val = subscriptions_dict[subscription_id]
+    #    lock.acquire()
+    #    try:
+    #        if 'next' not in subscription_val:
+    #            subscription_val['next'] = update['object_id']
+    #        subscription_val['last'] = update['object_id']
+    #        subscription_val['counter'] += 1
+    #        if subscription_val['counter'] > THRESHOLD:
+    #            retrieve_recent_geo(subscription_val)
+    #    finally:
+    #        lock.release()
+
+@app.route('/list')
+def list():
+    """
+    List all current subscriptions
+    """
+    api = client.InstagramAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    subscriptions = api.list_subscriptions()
+
+    return Response("You have the following subscriptions: " + subscriptions)
+
+@app.route('/unsubscribe')
+def unsubscribe():
+    """
+    Unsubscribe from all subscriptions
+    """
+    api = client.InstagramAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    try:
+        subscriptions = api.list_subscriptions()
+        for subscription in subscriptions:
+            api.delete_subscriptions(id=subscription)
+    except Exception as error:
+        print error
+
+    return Response("Unsubscribed from all subscriptions")
 
 def retrieve_recent_geo(subscription_val):
     """
-    Do batch retrieval of many instagram medias
+    Do batch retrieval of many different instagram medias
     """
-    insert_query = """INSERT INTO media_events (
+    insert_query = """INSERT IGNORE INTO media_events (
                         'user_name', 'user_id', 'tags', 'location_name', 'location_lat',
                         'location_lng', 'filter', 'created_time', 'image_url', 'media_url',
                         'text') VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
     recent_media = {}
-    api = client.InstagramAPI(access_token=g.access_token)
+    api = client.InstagramAPI(access_token=ACCESS_TOKEN)
     while True:
         new_recent_media, next = api.geography_recent_media(id=id, min_id=subscriptions_dict[id]['next'], count=subscription_val['counter'])
         if new_recent_media['pagination']['next_max_id'] > subscription_val['last']:
@@ -141,10 +174,13 @@ def before_request():
     """
     Connect to the database
     """
-    g.db = connect_db()
+    g._database = connect_db()
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """
+    Teardown the database after a request
+    """
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
@@ -164,10 +200,12 @@ def on_callback():
         print "access token= " + access_token
         if not access_token:
             return Response('Could not get access token')
-        g.access_token = access_token
 
-    except Exception:
-        print "Didn't get the access token."
+    except Exception, e:
+        logging.getLogger(__name__).error(e)
+        return Response("Didn't get the access token.")
+
+    return Response("Fine.")
 
 @app.route('/realtime_callback')
 def on_realtime_callback():
@@ -191,4 +229,4 @@ def on_realtime_callback():
     return Response('Parsed instagram')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8130)
+    app.run(host='0.0.0.0', port=8130, debug=True)
