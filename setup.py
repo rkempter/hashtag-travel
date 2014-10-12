ff5b60ace9a42febc14e5f4d2359e68b3049ee06
rkempter <renato.kempter@gmail.com>
2014-10-12 14:10:00 +0200

    Setup file for gunicorn

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "InstaCollector",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ['flask', 'python-instagram', 'mysql-python'],
+    install_requires = ['flask', 'python-instagram', 'mysql-python', 'foursquare', 'gunicorn'],
 
     author = "EPFL - Renato Kempter",
     author_email = "renato.kempter@gmail.com",
