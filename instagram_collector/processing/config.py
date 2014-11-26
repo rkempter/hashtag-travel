e43c9028fdee202ebfd83d00fe80addf7bd51108
rkempter <renato.kempter@gmail.com>
2014-11-27 00:16:06 +0100

    The processing config file

---
+++ instagram_collector/processing/config.py
@@ -0,0 +1,11 @@
+"""
+Config file that defines some
+of the configuration parameters
+"""
+
+# The number of topics (defines also the number of features for k-means clustering
+TOPIC_NBR= 100
+
+# The number of location clusters
+LOCATION_CLUSTER_NBR = 21
+
