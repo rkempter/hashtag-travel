5a74ba1854b4042bc72054b7474ae2476a81b81a
rkempter <renato.kempter@gmail.com>
2014-12-05 09:03:59 +0100

    check if list

--- instagram_collector/processing/generate_sets.py
+++ instagram_collector/processing/generate_sets.py
@@ -31,7 +31,7 @@ def get_feature_matrix(cluster_collection, topic_nbr=TOPIC_NBR):
 
     for index, document in enumerate(documents):
         doc_mapping.append(document["_id"])
-        if "distribution" not in document and not isinstance(document['distribution'], dict):
+        if "distribution" not in document and not isinstance(document['distribution'], list):
             continue
 
         features[index] = [float(value) for value in document['distribution']]
